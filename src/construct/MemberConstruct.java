2dcc25f7b38ce6e4d02c5eda06a3fa9948c38f3d
yuuuuuni <yuuuuuni930302@gmail.com>
2026-04-21 01:18:30 +0900

    생성자 - 도입

---
+++ src/construct/MemberConstruct.java
@@ -0,0 +1,15 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    //생성자
+    MemberConstruct(String name, int age, int grade) {
+        System.out.println("생성자 호출 name = " + name + ", age = " + age + ", grade = " + grade);
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
