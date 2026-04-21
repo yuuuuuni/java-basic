05de771a8ff7c52875d20b968fd507226e558597
yuuuuuni <yuuuuuni930302@gmail.com>
2026-04-22 00:23:31 +0900

    생성자 - 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,11 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //생성자 추가(생성자도 오버로딩을 할 수 있다.)
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); //변경
+    }
+
     //생성자
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name = " + name + ", age = " + age + ", grade = " + grade);
