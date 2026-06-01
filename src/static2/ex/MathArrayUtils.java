951e1d1f453d4827046dbd02054099033837c5a5
yuuuuuni <yuuuuuni930302@gmail.com>
2026-06-01 23:57:24 +0900

    문제와 풀이

---
+++ src/static2/ex/MathArrayUtils.java
@@ -0,0 +1,40 @@
+package static2.ex;
+
+public class MathArrayUtils {
+
+    private MathArrayUtils() {
+        //인스턴스 생성을 못하게 막으려면 생성자를 private으로 막으면 됨
+    }
+
+    public static int sum(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static double average(int[] values) {
+        return (double) sum(values) / values.length;
+    }
+
+    public static int min(int[] values) {
+        int minValue = values[0];
+        for (int value : values) {
+            if (value < minValue) {
+                minValue = value;
+            }
+        }
+        return minValue;
+    }
+
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int value : values) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+}
